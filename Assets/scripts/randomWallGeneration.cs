597c4c97bb34d1b27a1d663f2b5b5789279e9f97
DevynW <116035427+DevynW@users.noreply.github.com>
2024-09-23 12:13:19 -0400

    Initial commit

---
+++ Assets/scripts/randomWallGeneration.cs
@@ -0,0 +1,31 @@
+using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class randomWallGeneration : MonoBehaviour
+{
+    [SerializeField] GameObject[] wallPrefabs;
+    [SerializeField] public static float time = 10f;
+    [SerializeField] float rate = 1f;
+    // Start is called before the first frame update
+    void Start()
+    {
+        InvokeRepeating("MakeWall", time, rate);
+    }
+
+    // Update is called once per frame
+    void Update()
+    {
+        
+        
+        
+    }
+
+    void MakeWall()
+    {
+        int wallNum = Random.Range(0, wallPrefabs.Length);
+        Instantiate(wallPrefabs[wallNum]);
+        Debug.Log("We're making walls!");
+    }
+    
+}
